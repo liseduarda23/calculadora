f3de491a10d422e2c81beb4136512079df2efc27
jeimesonrf <jeimesonrf@gmail.com>
2023-06-19 19:46:47 -0300

    Referencia para os alunos

---
+++ main.py
@@ -0,0 +1,9 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.B('7'),sg.B('8'),sg.B('9'),sg.B('/')],
+    [sg.B('4'),sg.B('5'),sg.B('6'),sg.B('*')],
+    [sg.B('1'),sg.B('2'),sg.B('3'),sg.B('-')],
+    [sg.B('.'),sg.B('0'),sg.B('='),sg.B('+')],
+]
+
